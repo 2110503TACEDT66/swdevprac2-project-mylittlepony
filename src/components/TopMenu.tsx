21b46855c3f525055d20e65f41df16d8af653cde
aiyapatv <6633288721@student.chula.ac.th>
2024-03-25 20:07:14 +0700

    add banner and top menu

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,37 +1,21 @@
-import Image from "next/image";
-import styles from './topmenu.module.css'
-import TopMenuItem from './TopMenuItem';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-import { Link } from '@mui/material';
+import TopMenuItem from "./TopMenuItem";
+import styles from "./topmenu.module.css"
 
-export default async function TopMenu() {
-    const session = await getServerSession(authOptions)
-
+export default function TopMenu () {
     return (
-        <div className = {styles.menucontainer}>
-            <Image src={'/img/logo.png'} className={styles.logoimg} alt='logo'
-            width={0} height={0} sizes='100vh' />
-            <TopMenuItem title='Select Car' pageRef='/car'/>
-            <TopMenuItem title='Reservations' pageRef='/reservations'/>
-            <TopMenuItem title='About' pageRef='/about'/>
-
-            <div className='flex flex-row absolute right-0 h-full'>
-            <TopMenuItem title='Cart' pageRef='/cart'/>
-            
-            {
-                session? <Link href="/api/auth/signout"><div className='flex items-center  h-full px-2 text-cyan-600 text-sm'>
-                    Sign-Out of { session.user?.name }</div> </Link>
-                : <Link href="/api/auth/signin"><div className='flex items-center  h-full px-2 text-cyan-600 text-sm'> 
-                Sign-In </div></Link>
-            }
-            
+        <div className={styles.menucontainer}>
+            <div className={styles.containerleft}>
+                <TopMenuItem title="MyLittlePony"/>
             </div>
-
-
+            <div className={styles.containercenter}>
+                <TopMenuItem title="HOME" pageRef="/"/>
+                <TopMenuItem title="ABOUT US" pageRef="/"/>
+                <TopMenuItem title="RESERVE" pageRef="/"/>
+            </div>
+            <div className={styles.containerright}>
+                <TopMenuItem title="explore" pageRef="/restaurants" imgSrc="/img/explore.png"/>
+                <TopMenuItem pageRef="/" imgSrc="/img/user.png" title="user"/>
+            </div>
         </div>
-    )
-
-
-
+    );
 }
