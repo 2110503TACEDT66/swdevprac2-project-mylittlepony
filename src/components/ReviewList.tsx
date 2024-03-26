4ae01ab12323e2f8107e7b35094a75bb2f9d7b82
Tanathorn Piyachart <tangutang.tnp@gmail.com>
2024-03-26 23:00:24 +0700

    add

--- src/components/ReviewList.tsx
+++ src/components/ReviewList.tsx
@@ -1,35 +1,36 @@
 import { useAppSelector } from "@/redux/store"
 import { useDispatch } from "react-redux"
 import { AppDispatch } from "@/redux/store"
-import removeReservation from "@/libs/removeReservation"
-import getReservations from "@/libs/getReservations"
+import removeReview from "@/libs/removeReview"
+import getReviews from "@/libs/getReviews"
 import { getSession, useSession } from "next-auth/react"
 import { useEffect, useState } from "react"
-import { ReservationJson, ReviewJson } from "../../interface"
+import { ReviewJson } from "../../interface"
 import { getServerSession } from "next-auth"
 import { authOptions } from "@/app/api/auth/[...nextauth]/route"
-import CancelButton from "./CancelButton"
 import styles from "./reservationlist.module.css"
 import { Rating } from "@mui/material"
+import CancelReview from "./CancelReview"
 
 export default async function ReviewList({reviewsJson}: {reviewsJson: ReviewJson}) {
     
-    const reservationsItem = await reviewsJson
+    const reviewsItem = await reviewsJson
 
     return (
         <main className={styles.page}>
         <>
-        { reservationsItem.data.length === 0 ? (
+        { reviewsItem.data.length === 0 ? (
             <div className="text-xl text-center">No Table  </div>
         )
         :
         (
-            reservationsItem.data.map((reservationItem: any) => (
-                <div className="bg-slate-200 rounded px-5 mx-5 py-5 my-4"
-                    key = { reservationItem._id }>
+            reviewsItem.data.map((reviewItem: any) => (
+                <div className="bg-slate-600 rounded px-5 mx-5 py-5 my-6"
+                    key = { reviewItem._id }>
                         <div className="text-md">Rating: <Rating name="rating" 
-                    defaultValue={reservationItem.rating} precision={0.5} readOnly /></div>
-                        <div className="text-md">Description: {reservationItem.description} </div>
+                    defaultValue={reviewItem.rating} precision={0.5} readOnly /></div>
+                        <div className="text-md">Description: {reviewItem.description} </div>
+                        <CancelReview reviewId={reviewItem._id}/>
                 </div>
             ))
         )}
